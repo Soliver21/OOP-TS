7fa25e759e829f862f95058946daf09dde0db6bf
oliver <feketeoliver2006@gmail.com>
2025-09-09 10:37:59 +0200

    vége mára

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+type BaseOp = "sum" | "multiplier" | "mode" | "print";
 class SeriesOfNumbers{
     //Attribútumok (tulajdonságok [az osztály változói]) - rejtettek (private)
     private _count : number; //számosság
@@ -19,8 +20,59 @@ class SeriesOfNumbers{
     get values(): readonly number[]{
         return this._list;
     }
-    set updateValues(arr: number[]): void{
+    set updateValues(arr: number[]){
         if(arr.length === 0) throw new Error("Bencus nem lehet üres lista");
-        if(arr.every(n => Number.isFinite(n)) )
+        if(arr.every(n => Number.isFinite(n) && n > 0 )) throw new Error("Minden elem pozitív egész legyen");
+        this._list = [...arr]; // A lista elemeinek másolata kerül a _list-be! Ez egy új array lesz.
+        this._value = Math.max(...arr)
+    }
+
+    get count(): number {
+        return this._count;
+    }
+    get value(): number{
+        return this._value;
+    }
+    mode(): number {
+        const statistic: Record<number,number> = {};
+        for (let n of this._list) {
+            statistic[n] = (statistic[n] ?? 0)+1;            
+        }
+        let best = this._list[0];
+        for(const k of Object.keys(statistic)){
+            const key = Number(k);
+            if (statistic[key]! > statistic[best!]!) {
+                best = key;
+            }
+        }
+        return best!;
+    }
+    sum(){return this._list.reduce((acc,num) => acc + num, 0)}
+    multiplier(){return this._list.reduce((acc,num) => acc * num,1)}
+    // Róter (switch-case): - sum, multiplier, mode, print
+    run(op: BaseOp){
+        switch(op){
+            case "sum":
+                console.log(`Összeg: ${this.sum()}`);
+                break
+            case "multiplier":
+                console.log(`Szorzat: ${this.multiplier()}`)
+                break
+            case "mode":
+                console.log(`Leggyakoribb érték: ${this.mode()}`)
+                break;
+            case "print":
+                console.log("Kiírás: "+ this.values.join(", "))
+                break;
+            default:
+                console.log("Helytelen művelet")
+                break;
+        }
+    }
+    static help() :void{
+        console.log("Elérhető műveletek: sum, multiplier, mode, print")
+    }
+    static rnd(max: number):number{
+        return Math.floor(Math.random()*max)+1;
     }
 }
